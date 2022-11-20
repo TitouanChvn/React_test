2ccbe712a0993648a100a41f6cf9f8418a5b787f
CHAUVINEAU Titouan <titouan.chauvineau@telecomnancy.eu>
2022-11-21 00:51:43 +0100

    starting a react app

---
+++ react_test/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello world</h1>
+        <h2> First react App</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
